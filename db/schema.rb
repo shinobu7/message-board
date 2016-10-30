400670d6e6782d8a0761561ff8df2d5bba4ce995
shinobu7 <shinobu7moon@gmail.com>
2016-10-30 12:46:58 +0000

    age

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20161030082011) do
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
+    t.string   "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "age"
   end
 
 end
